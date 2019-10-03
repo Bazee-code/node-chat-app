23821c8b1d4f89c91327213b07244671b0e903a7
Bazee-code <52162434+Bazee-code@users.noreply.github.com>
2019-10-03 23:08:06 +0300

    'addconnectanddisconnecteventhandlers'

--- server/server.js
+++ server/server.js
@@ -1,18 +1,31 @@
+//integrating socket.io with our server
+// socket.io allows for connection to and from client (browser)and server
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
-var app = express();
 //make our port dynamic for heroku
 const port = process.env.PORT || 3000;
+
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 //middleware for our static webpage
 app.use(express.static(path.join(__dirname,"../public")));
 
-// app.get('/',(req,res)=>{
-// 	res.send(index.html);
-// });
+//register an event listener
+io.on('connection',(socket)=>{
+	console.log("New user connected!");
+	//registering our event
+	socket.on('disconnect',()=>{
+		console.log("User disconnected!");
+	});
+});
 
-app.listen(port,()=>{
+
+server.listen(port,()=>{
 	console.log(`Server is up on port ${port}!`);
 });
 
